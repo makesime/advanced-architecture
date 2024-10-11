6b1f31882044d163132aa2a5201471c2947986db
Maxime Tessier <mtessier@petalmd.com>
2024-10-11 14:59:28 -0400

    Autowire event classes

--- src/alarms/domain/events/alarm-created.event.ts
+++ src/alarms/domain/events/alarm-created.event.ts
@@ -1,5 +1,7 @@
 import { Alarm } from '../alarm';
+import { AutoWiredEvent } from '../../../shared/decorators/autowired-event.decorator';
 
+@AutoWiredEvent
 export class AlarmCreatedEvent {
   constructor(public readonly alarm: Alarm) {}
 }
